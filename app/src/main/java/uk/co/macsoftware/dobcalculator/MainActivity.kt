322a726d6fbfc78ada2a0a61547afb7ceec93637
Ian MacKinnon <87543117+macsoftware@users.noreply.github.com>
2022-10-27 20:17:41 +0100

    Feature/start app (#2)
    * Finishing calculations and doing some refactoring.
    
    * Finishing calculations and doing some refactoring.
    
    * Creating a constants object

--- app/src/main/java/uk/co/macsoftware/dobcalculator/MainActivity.kt
+++ app/src/main/java/uk/co/macsoftware/dobcalculator/MainActivity.kt
@@ -3,36 +3,75 @@ package uk.co.macsoftware.dobcalculator
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.Toast
-import java.util.Calendar
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+    private var textViewSelectedDate: TextView? = null
+    private var textViewAgeInMinutes: TextView? = null
+    private var textViewAgeInMinutesHeader: TextView? = null
+
+    private fun init() {
         val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
+        textViewSelectedDate = findViewById(R.id.textViewSelectedDate)
+        textViewSelectedDate?.text = ""
+
+        textViewAgeInMinutes = findViewById(R.id.textViewAgeInMinutes)
+        textViewAgeInMinutes?.text = ""
+
+        textViewAgeInMinutesHeader = findViewById(R.id.textViewAgeInMinutesHeader)
 
         buttonDatePicker.setOnClickListener {
             clickDatePicker()
         }
     }
 
-    fun clickDatePicker() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun clickDatePicker() {
 
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
-            { view, year, month, dayOfMonth ->
-                Toast.makeText(this, "DatePicker works", Toast.LENGTH_LONG).show()
-            }, year, month, day
-        ).show()
+            { _, selectedYear, selectedMonth, selectedDayOfMonth ->
 
-        Toast.makeText(this, "buttonDatePicker pressed", Toast.LENGTH_LONG).show()
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                textViewSelectedDate?.text = selectedDate
+
+                val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = simpleDateFormat.parse(selectedDate)
+
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time / Constants.SIXTY_THOUSAND
+
+                    val currentDate =
+                        simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time / Constants.SIXTY_THOUSAND
+
+                        val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                        textViewAgeInMinutesHeader?.visibility = View.VISIBLE
+                        textViewAgeInMinutes?.text = differenceInMinutes.toString()
+                    }
+                }
+            }, year, month, day
+        )
+
+        dpd.datePicker.maxDate = System.currentTimeMillis() - Constants.EIGHT_SIX_FOUR_FIVEZEROS
+        dpd.show()
     }
 }
